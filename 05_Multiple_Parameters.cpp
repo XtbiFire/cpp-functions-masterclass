11a5b641c142b4b5b7866f33fddfbd30c03bd6a6
XtbiFire <xtbifire@gmail.com>
2026-06-17 20:52:39 +0530

    Create 05_Multiple_Parameters.cpp

---
+++ 05_Multiple_Parameters.cpp
@@ -0,0 +1,20 @@
+/*
+Topic: Multiple Parameters
+Author: Imran
+*/
+
+#include <iostream>
+using namespace std;
+
+void show(int a, int b)
+{
+    cout << a;
+    cout << b;
+}
+
+int main()
+{
+    show(5, 7);
+
+    return 0;
+}

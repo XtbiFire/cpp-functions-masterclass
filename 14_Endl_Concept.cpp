2a07264f3757cf3692e10feb3375ded70ce0a409
XtbiFire <xtbifire@gmail.com>
2026-06-17 20:56:07 +0530

    Create 14_Endl_Concept.cpp

---
+++ 14_Endl_Concept.cpp
@@ -0,0 +1,21 @@
+/*
+Topic: endl
+Author: Imran
+
+endl moves cursor to next line
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "A";
+    cout << "B";
+
+    cout << endl;
+
+    cout << "C";
+
+    return 0;
+}

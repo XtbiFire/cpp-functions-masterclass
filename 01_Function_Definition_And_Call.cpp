6bcce1ca1d1f6aeacecd66826f1e0f36ebebe548
XtbiFire <xtbifire@gmail.com>
2026-06-17 20:50:44 +0530

    Create 01_Function_Definition_And_Call.cpp

---
+++ 01_Function_Definition_And_Call.cpp
@@ -0,0 +1,24 @@
+/*
+Topic: Function Definition And Call
+Author: Imran
+
+Concept:
+Create a function and call it from main().
+*/
+
+#include <iostream>
+using namespace std;
+
+// Function Definition
+void hello()
+{
+    cout << "Hello";
+}
+
+int main()
+{
+    // Function Call
+    hello();
+
+    return 0;
+}

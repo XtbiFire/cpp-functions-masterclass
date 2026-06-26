3654022bf5ad5d8adf7ba7b7f714579098825e76
XtbiFire <xtbifire@gmail.com>
2026-06-26 17:11:44 +0530

    Create 27_what_is_prototype.cpp

---
+++ 27_what_is_prototype.cpp
@@ -0,0 +1,29 @@
+/*
+◆──────────────────────────◆
+      27. What Is a Prototype
+◆──────────────────────────◆
+
+💡 Remember
+Prototype = Function Declaration.
+
+*/
+
+#include <iostream>
+using namespace std;
+
+// Prototype
+void display();
+
+// Main Function
+int main()
+{
+    display();
+
+    return 0;
+}
+
+// Function Definition
+void display()
+{
+    cout << "Prototype Example";
+}

157fdf8dde25b46ccedff99b6f3fb30ec6f463f5
XtbiFire <xtbifire@gmail.com>
2026-06-26 18:16:08 +0530

    Update 23_Local_Vs_Global.cpp

--- 23_Local_Vs_Global.cpp
+++ 23_Local_Vs_Global.cpp
@@ -1,21 +1,33 @@
 /*
-Topic: Local Vs Global Variable
-Author: Imran
+◆──────────────────────────◆
+23. Local Vs Global Variable
+◆──────────────────────────◆
 
-Local variable gets priority
-over global variable.
+💡 Remember
+A local variable gets
+priority over a
+global variable.
+
 */
 
 #include <iostream>
 using namespace std;
 
+// Global Variable
 int x = 100;
 
+// Main Function
 int main()
 {
+    // Local Variable
     int x = 10;
 
     cout << x;
 
     return 0;
 }
+
+/*
+Output:
+10
+*/

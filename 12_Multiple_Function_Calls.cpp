9a30a955cabbec0522f5a479e73b85e6c3abdb13
XtbiFire <xtbifire@gmail.com>
2026-06-26 18:03:32 +0530

    Update 12_Multiple_Function_Calls.cpp

--- 12_Multiple_Function_Calls.cpp
+++ 12_Multiple_Function_Calls.cpp
@@ -1,19 +1,33 @@
 /*
-Topic: Multiple Function Calls
-Author: Imran
+◆──────────────────────────◆
+12. Multiple Function Calls
+◆──────────────────────────◆
+
+💡 Remember
+A function can be
+called multiple times.
+
 */
 
 #include <iostream>
 using namespace std;
 
+// Function Definition
 int add(int a, int b)
 {
     return a + b;
 }
 
+// Main Function
 int main()
 {
+    // Function Calls
     cout << add(2, 3) + add(4, 5);
 
     return 0;
 }
+
+/*
+Output:
+14
+*/

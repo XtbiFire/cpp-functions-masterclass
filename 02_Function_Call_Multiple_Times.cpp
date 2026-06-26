f50ed43e6ca22c816cabcfdc81ec77771739f4ae
XtbiFire <xtbifire@gmail.com>
2026-06-26 17:48:28 +0530

    Update 02_Function_Call_Multiple_Times.cpp

--- 02_Function_Call_Multiple_Times.cpp
+++ 02_Function_Call_Multiple_Times.cpp
@@ -1,24 +1,35 @@
 /*
-Topic: Multiple Function Calls
-Author: Imran
+◆──────────────────────────◆
+02. Multiple Function Calls
+◆──────────────────────────◆
 
-Concept:
-One function can be called many times.
+💡 Remember
+One function can be
+called multiple times.
+
 */
 
 #include <iostream>
 using namespace std;
 
+// Function Definition
 void hello()
 {
     cout << "Hello ";
 }
 
+// Main Function
 int main()
 {
+    // Function Calls
     hello();
     hello();
     hello();
 
     return 0;
 }
+
+/*
+Output:
+Hello Hello Hello
+*/

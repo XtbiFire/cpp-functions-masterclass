36ed47c31b589a99b5933c4f77458b129825d21b
XtbiFire <xtbifire@gmail.com>
2026-06-26 18:02:36 +0530

    Update 11_Square_Function.cpp

--- 11_Square_Function.cpp
+++ 11_Square_Function.cpp
@@ -1,19 +1,33 @@
 /*
-Topic: Square Function
-Author: Imran
+◆──────────────────────────◆
+11. Square Function
+◆──────────────────────────◆
+
+💡 Remember
+A function can return
+a calculated value.
+
 */
 
 #include <iostream>
 using namespace std;
 
+// Function Definition
 int square(int n)
 {
     return n * n;
 }
 
+// Main Function
 int main()
 {
+    // Function Call
     cout << square(4);
 
     return 0;
 }
+
+/*
+Output:
+16
+*/

427dae2d1f66673f7cc2c5bca12ade2531dadf9a
XtbiFire <xtbifire@gmail.com>
2026-06-26 17:57:19 +0530

    Update 07_Return_Function.cpp

--- 07_Return_Function.cpp
+++ 07_Return_Function.cpp
@@ -1,21 +1,33 @@
 /*
-Topic: Return Function
-Author: Imran
+◆──────────────────────────◆
+07. Return Function
+◆──────────────────────────◆
 
-return sends value back to caller
+💡 Remember
+Return sends a value
+back to the caller.
+
 */
 
 #include <iostream>
 using namespace std;
 
+// Function Definition
 int add(int a, int b)
 {
     return a + b;
 }
 
+// Main Function
 int main()
 {
+    // Function Call
     cout << add(2, 3);
 
     return 0;
 }
+
+/*
+Output:
+5
+*/

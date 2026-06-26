3fdedbea70239005a4dad85760e0c4d83703475c
XtbiFire <xtbifire@gmail.com>
2026-06-26 18:08:29 +0530

    Update 16_Function_As_Argument.cpp

--- 16_Function_As_Argument.cpp
+++ 16_Function_As_Argument.cpp
@@ -1,22 +1,34 @@
 /*
-Topic: Function As Argument
-Author: Imran
+◆──────────────────────────◆
+16. Function As Argument
+◆──────────────────────────◆
 
-Returned value of one function
-can be passed as argument to another function.
+💡 Remember
+A function's returned value
+can be passed as an argument
+to another function.
+
 */
 
 #include <iostream>
 using namespace std;
 
+// Function Definition
 int add(int a, int b)
 {
     return a + b;
 }
 
+// Main Function
 int main()
 {
+    // Function Calls
     cout << add(add(1,2), add(3,4));
 
     return 0;
 }
+
+/*
+Output:
+10
+*/

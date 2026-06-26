7f3020b55c927389ea8babe41810c8d61a43c063
XtbiFire <xtbifire@gmail.com>
2026-06-26 18:15:09 +0530

    Update 22_Global_Variable.cpp

--- 22_Global_Variable.cpp
+++ 22_Global_Variable.cpp
@@ -1,24 +1,36 @@
 /*
-Topic: Global Variable
-Author: Imran
+◆──────────────────────────◆
+22. Global Variable
+◆──────────────────────────◆
 
-Variable declared outside all functions
-can be accessed by all functions.
+💡 Remember
+A global variable can be
+accessed by all functions.
+
 */
 
 #include <iostream>
 using namespace std;
 
+// Global Variable
 int x = 100;
 
+// Function Definition
 void show()
 {
     cout << x;
 }
 
+// Main Function
 int main()
 {
+    // Function Call
     show();
 
     return 0;
 }
+
+/*
+Output:
+100
+*/

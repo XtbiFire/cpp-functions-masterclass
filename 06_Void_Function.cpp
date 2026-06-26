de7fa141b20719b1aa1e9ad5441b937271acee32
XtbiFire <xtbifire@gmail.com>
2026-06-26 17:55:50 +0530

    Update 06_Void_Function.cpp

--- 06_Void_Function.cpp
+++ 06_Void_Function.cpp
@@ -1,22 +1,33 @@
 /*
-Topic: Void Function
-Author: Imran
+◆──────────────────────────◆
+06. Void Function
+◆──────────────────────────◆
 
-void = Returns nothing
-But can still print output
+💡 Remember
+Void returns nothing,
+but can print output.
+
 */
 
 #include <iostream>
 using namespace std;
 
+// Function Definition
 void add(int a, int b)
 {
     cout << a + b;
 }
 
+// Main Function
 int main()
 {
+    // Function Call
     add(2, 3);
 
     return 0;
 }
+
+/*
+Output:
+5
+*/

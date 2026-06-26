369cdede2bf322e8cba58ebea98a73007ad3b9c2
XtbiFire <xtbifire@gmail.com>
2026-06-26 17:31:53 +0530

    Create 46_conversion.cpp

---
+++ 46_conversion.cpp
@@ -0,0 +1,32 @@
+/*
+◆──────────────────────────◆
+        46. Conversion
+◆──────────────────────────◆
+
+💡 Remember
+If no exact match exists,
+compiler performs conversion.
+
+*/
+
+#include <iostream>
+using namespace std;
+
+// Function
+void show(float x)
+{
+    cout << "Float";
+}
+
+// Main Function
+int main()
+{
+    show(10);
+
+    return 0;
+}
+
+/*
+Output:
+Float
+*/

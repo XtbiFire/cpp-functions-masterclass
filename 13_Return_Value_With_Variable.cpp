e0c4395970f7e380c15ab6f60e2464e7ef401734
XtbiFire <xtbifire@gmail.com>
2026-06-26 18:04:31 +0530

    Update 13_Return_Value_With_Variable.cpp

--- 13_Return_Value_With_Variable.cpp
+++ 13_Return_Value_With_Variable.cpp
@@ -1,21 +1,36 @@
 /*
-Topic: Return Value With Variable
-Author: Imran
+◆──────────────────────────◆
+13. Return Value With Variable
+◆──────────────────────────◆
+
+💡 Remember
+A returned value can be
+stored and used again.
+
 */
 
 #include <iostream>
 using namespace std;
 
+// Function Definition
 int add(int a, int b)
 {
     return a + b;
 }
 
+// Main Function
 int main()
 {
+    // Store Return Value
     int x = add(2, 3);
 
+    // Function Call
     cout << x + add(1, 1);
 
     return 0;
 }
+
+/*
+Output:
+7
+*/

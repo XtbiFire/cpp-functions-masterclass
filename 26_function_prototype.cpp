062f0b2175cf2c7f9c012b75c68c120eafe3d52d
XtbiFire <xtbifire@gmail.com>
2026-06-26 17:10:32 +0530

    Create 26_function_prototype.cpp

---
+++ 26_function_prototype.cpp
@@ -0,0 +1,30 @@
+/*
+◆──────────────────────────◆
+      26. Function Prototype
+◆──────────────────────────◆
+
+💡 Remember
+Prototype = Information
+Definition = Implementation
+
+*/
+
+#include <iostream>
+using namespace std;
+
+// Prototype
+void greet();
+
+// Main Function
+int main()
+{
+    greet();
+
+    return 0;
+}
+
+// Function Definition
+void greet()
+{
+    cout << "Hello, Imran!";
+}

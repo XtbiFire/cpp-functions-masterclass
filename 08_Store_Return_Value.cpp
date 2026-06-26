93f72e6e7abd05c499cd8ba59e09b9a08e9fa056
XtbiFire <xtbifire@gmail.com>
2026-06-26 17:58:19 +0530

    Update 08_Store_Return_Value.cpp

--- 08_Store_Return_Value.cpp
+++ 08_Store_Return_Value.cpp
@@ -1,21 +1,35 @@
 /*
-Topic: Store Return Value
-Author: Imran
+◆──────────────────────────◆
+08. Store Return Value
+◆──────────────────────────◆
+
+💡 Remember
+A returned value can be
+stored in a variable.
+
 */
 
 #include <iostream>
 using namespace std;
 
+// Function Definition
 int add(int a, int b)
 {
     return a + b;
 }
 
+// Main Function
 int main()
 {
+    // Store Return Value
     int result = add(2, 3);
 
     cout << result;
 
     return 0;
 }
+
+/*
+Output:
+5
+*/

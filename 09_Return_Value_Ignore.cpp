64d8f4c2c3f905a6eddc645b6016e9502790884b
XtbiFire <xtbifire@gmail.com>
2026-06-26 17:59:30 +0530

    Update 09_Return_Value_Ignore.cpp

--- 09_Return_Value_Ignore.cpp
+++ 09_Return_Value_Ignore.cpp
@@ -1,22 +1,33 @@
 /*
-Topic: Ignore Return Value
-Author: Imran
+◆──────────────────────────◆
+09. Ignore Return Value
+◆──────────────────────────◆
 
-Returned value is ignored.
-Nothing is printed.
+💡 Remember
+A function can return a value,
+but you may choose to ignore it.
+
 */
 
 #include <iostream>
 using namespace std;
 
+// Function Definition
 int show()
 {
     return 5;
 }
 
+// Main Function
 int main()
 {
+    // Function Call
     show();
 
     return 0;
 }
+
+/*
+Output:
+No Output
+*/

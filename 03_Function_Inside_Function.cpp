5f0dad82e959100caff3df9aa59458baf15afedf
XtbiFire <xtbifire@gmail.com>
2026-06-26 17:50:03 +0530

    Update 03_Function_Inside_Function.cpp

--- 03_Function_Inside_Function.cpp
+++ 03_Function_Inside_Function.cpp
@@ -1,28 +1,40 @@
 /*
-Topic: Function Inside Function
-Author: Imran
+◆──────────────────────────◆
+03. Function Inside Function
+◆──────────────────────────◆
 
-Concept:
-A function can call another function.
+💡 Remember
+One function can
+call another function.
+
 */
 
 #include <iostream>
 using namespace std;
 
+// Function Definition
 void first()
 {
     cout << "A";
 }
 
+// Function Definition
 void second()
 {
     first();
     cout << "B";
 }
 
+// Main Function
 int main()
 {
+    // Function Call
     second();
 
     return 0;
 }
+
+/*
+Output:
+AB
+*/

ef093866e784f6a8f2f9b6b1adc7348fd2cb0d55
XtbiFire <xtbifire@gmail.com>
2026-06-26 18:09:19 +0530

    Update 17_Function_Chaining.cpp

--- 17_Function_Chaining.cpp
+++ 17_Function_Chaining.cpp
@@ -1,22 +1,34 @@
 /*
-Topic: Function Chaining
-Author: Imran
+◆──────────────────────────◆
+17. Function Chaining
+◆──────────────────────────◆
 
-One function call is passed
-inside another function call.
+💡 Remember
+One function call can be
+passed inside another
+function call.
+
 */
 
 #include <iostream>
 using namespace std;
 
+// Function Definition
 int add(int a, int b)
 {
     return a + b;
 }
 
+// Main Function
 int main()
 {
+    // Function Calls
     cout << add(add(1, 2), 3);
 
     return 0;
 }
+
+/*
+Output:
+6
+*/

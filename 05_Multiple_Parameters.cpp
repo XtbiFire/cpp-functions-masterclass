bd2253cf98d51d8cb04b78a64c8a3871bb4bb868
XtbiFire <xtbifire@gmail.com>
2026-06-26 17:53:22 +0530

    Update 05_Multiple_Parameters.cpp

--- 05_Multiple_Parameters.cpp
+++ 05_Multiple_Parameters.cpp
@@ -1,20 +1,34 @@
 /*
-Topic: Multiple Parameters
-Author: Imran
+◆──────────────────────────◆
+05. Multiple Parameters
+◆──────────────────────────◆
+
+💡 Remember
+One function can have
+multiple parameters.
+
 */
 
 #include <iostream>
 using namespace std;
 
+// Function Definition
 void show(int a, int b)
 {
     cout << a;
     cout << b;
 }
 
+// Main Function
 int main()
 {
+    // Function Call
     show(5, 7);
 
     return 0;
 }
+
+/*
+Output:
+57
+*/

99162353e65f4efe6fad19e7553f7e7845568fce
XtbiFire <xtbifire@gmail.com>
2026-06-28 13:20:48 +0530

    Create 63_returning_int.cpp

---
+++ 63_returning_int.cpp
@@ -0,0 +1,40 @@
+/*
+◆──────────────────────────◆
+      63. Returning int
+◆──────────────────────────◆
+
+💡 Remember
+
+An int function must
+return an integer value.
+
+The returned value can be
+a number, a variable or
+an expression.
+
+*/
+
+#include <iostream>
+using namespace std;
+
+// Function
+int getNumber()
+{
+    return 100;
+}
+
+// Main Function
+int main()
+{
+    cout << "Number : " << getNumber();
+
+    return 0;
+}
+
+/*
+
+Output
+
+Number : 100
+
+*/

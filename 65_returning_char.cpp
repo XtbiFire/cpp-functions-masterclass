dbaba714e1cbc3414a3402d8b38cb39545b0d85a
XtbiFire <xtbifire@gmail.com>
2026-06-28 13:23:18 +0530

    Create 65_returning_char.cpp

---
+++ 65_returning_char.cpp
@@ -0,0 +1,39 @@
+/*
+◆──────────────────────────◆
+      65. Returning Char
+◆──────────────────────────◆
+
+💡 Remember
+
+A char function returns
+a single character value.
+
+Character values are written
+inside single quotes (' ').
+
+*/
+
+#include <iostream>
+using namespace std;
+
+// Function
+char getGrade()
+{
+    return 'A';
+}
+
+// Main Function
+int main()
+{
+    cout << "Grade : " << getGrade();
+
+    return 0;
+}
+
+/*
+
+Output
+
+Grade : A
+
+*/

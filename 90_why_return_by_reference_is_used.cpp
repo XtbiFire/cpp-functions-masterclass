b5b8fae8456161a8eca693564dee6463d1d12405
XtbiFire <xtbifire@gmail.com>
2026-07-10 10:40:52 +0530

    Create 90_why_return_by_reference_is_used.cpp

---
+++ 90_why_return_by_reference_is_used.cpp
@@ -0,0 +1,48 @@
+/*
+◆────────────────────────────────────────◆
+90. Why Return by Reference Is Used
+◆────────────────────────────────────────◆
+
+💡 Remember
+
+Return by Reference allows
+the original variable to be
+modified without creating
+an extra copy.
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int marks = 90;
+
+// Function
+int& getMarks()
+{
+    return marks;
+}
+
+// Main Function
+int main()
+{
+    getMarks() += 5;
+
+    cout << marks;
+
+    return 0;
+}
+
+/*
+
+Output:
+
+95
+
+Time Complexity:
+O(1)
+
+Space Complexity:
+O(1)
+
+*/

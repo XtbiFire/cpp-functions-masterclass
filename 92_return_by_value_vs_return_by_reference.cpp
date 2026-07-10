69f068011c433d3dcafcfe619d9954667da1cc11
XtbiFire <xtbifire@gmail.com>
2026-07-10 10:41:39 +0530

    Create 92_return_by_value_vs_return_by_reference.cpp

---
+++ 92_return_by_value_vs_return_by_reference.cpp
@@ -0,0 +1,61 @@
+/*
+◆────────────────────────────────────────◆
+92. Return by Value vs Return by Reference
+◆────────────────────────────────────────◆
+
+💡 Remember
+
+Return by Value creates
+a copy.
+
+Return by Reference
+works on the original
+variable.
+
+*/
+
+#include <iostream>
+using namespace std;
+
+// Return by Value
+int value(int x)
+{
+    return x;
+}
+
+// Return by Reference
+int& reference(int &x)
+{
+    return x;
+}
+
+// Main Function
+int main()
+{
+    int a = 20;
+    int b = 20;
+
+    // value(a) = 50;   // ❌ Compiler Error
+
+    reference(b) = 50;
+
+    cout << a << endl;
+    cout << b;
+
+    return 0;
+}
+
+/*
+
+Output:
+
+20
+50
+
+Time Complexity:
+O(1)
+
+Space Complexity:
+O(1)
+
+*/
